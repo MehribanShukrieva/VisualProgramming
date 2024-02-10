e785a78dae1439f467da8d0c0a1f9146b2e4e694
Mehriban Shukrieva <127889525+marynotfound@users.noreply.github.com>
2024-02-10 13:57:56 +0300

    Add files via upload

---
+++ DateTime2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Main
+{
+    public static void Main()
+    {
+        // Get the current date and time
+        System.DateTime today = System.DateTime.Now;
+
+        // Display the current date and time
+        System.Console.WriteLine("Today = " + System.DateTime.Now);
+
+        // Define a TimeSpan representing a duration of 40 days
+        System.TimeSpan duration = new System.TimeSpan(40, 0, 0, 0);
+
+        // Add the defined duration (40 days) to the current date
+        System.DateTime answer = today.Add(duration);
+
+        // Display the day of the week for the resulting date
+        System.Console.WriteLine("{0:dddd}", answer);
+    }
+}

3d93be89625a65416b68de87c300102f3e787589
Mehriban Shukrieva <127889525+marynotfound@users.noreply.github.com>
2024-02-13 16:25:07 +0300

    Add files via upload

---
+++ IdentifyMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Input a number:");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            // Create a square matrix of size n x n filled with zeroes except for diagonal elements (identity matrix)
+            var M =
+                Enumerable.Range(0, n)
+                    .Select(i =>
+                        Enumerable.Repeat(0, n)
+                            .Select((z, j) => j == i ? 1 : 0) // Sets diagonal elements to 1, rest to 0
+                            .ToList()
+                    )
+                    .ToList();
+
+            // Display the matrix (identity matrix) generated above
+            foreach (var row in M)
+            {
+                foreach (var element in row)
+                {
+                    Console.Write(" " + element); // Print each element of the matrix row
+                }
+                Console.WriteLine(); // Move to the next line after printing each row
+            }
+        }
+    }
+}

f3284b9632b010350a2c78a453c9c3b8401e587b
Mehriban Shukrieva <127889525+marynotfound@users.noreply.github.com>
2024-02-13 16:28:55 +0300

    Add files via upload

---
+++ FirstLastElementArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Exercise49
+{
+    public static void Main()
+    {
+        
+        int[] nums1 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+        Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums1));
+
+        // Define another array of integers 'nums2' with pre-defined values
+        int[] nums2 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5 };
+
+        // Display the elements of 'nums2' array using string.Join to concatenate them
+        Console.WriteLine("\nArray2: [{0}]", string.Join(", ", nums2));
+
+        Console.WriteLine("\nCheck if the first element or the last element of the two arrays (length 1 or more) are equal.");
+
+      
+        Console.WriteLine((nums1[0].Equals(nums2[0])) || (nums1[nums1.Length - 1].Equals(nums2[nums2.Length - 1])));
+    }
+}
